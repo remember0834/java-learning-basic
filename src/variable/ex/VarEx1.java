8ad232f3d5831979896d9009bdee1b9c80f8c59e
remember0834 <jiehyuk@naver.com>
2025-09-17 16:43:26 +0900

    0917variable clear

---
+++ src/variable/ex/VarEx1.java
@@ -0,0 +1,16 @@
+package variable.ex;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+        /*
+        System.out.println(4 + 3);
+        System.out.println(4 - 3);
+        System.out.println(4 * 3);
+        */
+        int num1 = 4, num2 = 3;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+
+    }
+}
